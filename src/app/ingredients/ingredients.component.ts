750e35643ab83c58bef7462adaa192846fc1a640
Mayada_yacoub@mcodekw.com <you@example.com>
2025-03-19 13:44:58 +0200

    Create ingredients service

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IngredientsWayStepsService } from '../services/ingredients-way-steps.service';
 
 @Component({
   selector: 'app-ingredients',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './ingredients.component.css'
 })
 export class IngredientsComponent {
+  ingredient!:any[]
+  noteSteps!:any[]
+  recipeSteps!:any[]
+  constructor(private   ingredientsWayStepsService:IngredientsWayStepsService){ }
+  ngOnInit(): void {
 
+    this.ingredientsWayStepsService.getDishIngredients(97, 69).subscribe({
+      next: (response) => {
+        console.log('Success:', response);
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+      complete: () => {
+        console.log('Request completed.');
+      }
+    });
+    
+  }
+  
 }
